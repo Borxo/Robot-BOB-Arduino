7387c317b0c3081dac9ece9e797a34eceb7e1d78
borxoloko <borxoloko@live.com>
2019-04-23 12:04:21 +0200

    Control_Todo o Nada

--- Control_Todo o Nada/Control_Todo o Nada/Control_Todo o Nada.ino
+++ Control_Todo o Nada/Control_Todo o Nada/Control_Todo o Nada.ino
@@ -1,5 +1,5 @@
 /*
- Name:		Control_Proporcional.ino
+ Name:		Control_Todo o Nada.ino
  Created:	07/04/2019 17:23:59
  Author:	Borxo García
  Proyect:   Robot BoB
@@ -17,17 +17,9 @@ const int enableI = 11;
 
 //Variables//
 int estado = 0;
-int error = 0;
-int errorPasado = 0;
-int Diferencia = 0;
-float control = 0;
-float control1 = 0;
 
 //Variables Control Porporcional y Derivativo//
 int velocidad = 90;
-float kp = 9;
-float kd = 15;
-
 
 
 void setup() {
@@ -36,7 +28,6 @@ void setup() {
 		pinMode(InputD[i], OUTPUT);
 		pinMode(InputI[i], OUTPUT);
 		pinMode(pin[i], INPUT);
-		//pinMode(led[i], OUTPUT);
 	}
 #ifdef DEBUG
 	Serial.begin(4800);
@@ -45,37 +36,7 @@ void setup() {
 
 void loop()
 {
-	errorPasado = error;
-
-#ifdef DEBUG
-	Serial.print("  ");
-	Serial.print("errorPasado:");
-	Serial.print(errorPasado);
-	Serial.print("  ");
-#endif // DEBUG
-
 	LecturaSensores();
-	Diferencia = error - errorPasado;
-
-#ifdef DEBUG
-	Serial.print("Diferencia:");
-	Serial.print(Diferencia);
-	Serial.print("  ");
-#endif // DEBUG
-
-	control = velocidad + ((error * kp) + (kd * Diferencia));
-	control1 = velocidad - ((error * kp) + (kd * Diferencia));
-	Motores();
-
-#ifdef DEBUG
-	Serial.print("control:");
-	Serial.print(control);
-	Serial.print("   ");
-	Serial.print("control1:");
-	Serial.print(control1);
-	Serial.print("   ");
-#endif
-
 }
 void LecturaSensores()
 {
@@ -94,156 +55,178 @@ void LecturaSensores()
 	switch (estado)
 	{
 	case 0B000000001:
-#ifdef DEBUG
-		Serial.print("+8  ");
-#endif // DEBUG
-		error = 8;
+		#ifdef DEBUG
+			Serial.print("+8  ");
+		#endif // DEBUG
+		GiroDerecha();
 		break;
 
 	case 0B000000011:
-#ifdef DEBUG
-		Serial.print("+7  ");
-#endif // DEBUG
-		error = 7;
+		#ifdef DEBUG
+			Serial.print("+7  ");
+		#endif // DEBUG
+		GiroDerecha();
 		break;
 
 	case 0B000000010:
-#ifdef DEBUG
-		Serial.print("+6  ");
-#endif // DEBUG
-		error = 6;
+		#ifdef DEBUG
+			Serial.print("+6  ");
+		#endif // DEBUG
+		GiroDerecha();
 		break;
 
 	case 0B000000110:
-#ifdef DEBUG
-		Serial.print("+5  ");
-#endif // DEBUG
-		error = 5;
+		#ifdef DEBUG
+			Serial.print("+5  ");
+		#endif // DEBUG
+		GiroDerecha();
 		break;
 
 	case 0B000000100:
-#ifdef DEBUG
-		Serial.print("+4  ");
-#endif // DEBUG
-		error = 4;
+		#ifdef DEBUG
+			Serial.print("+4  ");
+		#endif // DEBUG
+		GiroDerecha();
 		break;
 
 	case 0B000001100:
-#ifdef DEBUG
-		Serial.print("+3  ");
-#endif // DEBUG
-		error = 3;
+		#ifdef DEBUG
+			Serial.print("+3  ");
+		#endif // DEBUG
+		GiroDerecha();
 		break;
 
 	case 0B000001000:
-#ifdef DEBUG
-		Serial.print("+2  ");
-#endif // DEBUG
-		error = 2;
+		#ifdef DEBUG
+			Serial.print("+2  ");
+		#endif // DEBUG
+		GiroDerecha();
 		break;
 
 	case 0B000011000:
-#ifdef DEBUG
+		#ifdef DEBUG
 		Serial.print("+1  ");
-#endif // DEBUG
-		error = 1;
+		#endif // DEBUG
+		GiroDerecha();
 		break;
 
 	case 0B000010000:
-#ifdef DEBUG
-		Serial.print("0  ");
-#endif // DEBUG
-		error = 0;
+		#ifdef DEBUG
+			Serial.print("0  ");
+		#endif // DEBUG
+		Avanzar();
 		break;
 
 	case 0B000110000:
-#ifdef DEBUG
-		Serial.print("-1  ");
-#endif // DEBUG
-		error = -1;
+		#ifdef DEBUG
+			Serial.print("-1  ");
+		#endif // DEBUG
+		GiroIzquierda();
 		break;
 
 	case 0B000100000:
-#ifdef DEBUG
-		Serial.print("-2  ");
-#endif // DEBUG
-		error = -2;
+		#ifdef DEBUG
+			Serial.print("-2  ");
+		#endif // DEBUG
+		GiroIzquierda();
 		break;
 
 	case 0B001100000:
-#ifdef DEBUG
-		Serial.print("-3  ");
-#endif // DEBUG
-		error = -3;
+		#ifdef DEBUG
+			Serial.print("-3  ");
+		#endif // DEBUG
+		GiroIzquierda();
 		break;
 
 	case 0B001000000:
-#ifdef DEBUG
-		Serial.print("-4  ");
-#endif // DEBUG
-		error = -4;
+		#ifdef DEBUG
+			Serial.print("-4  ");
+		#endif // DEBUG
+		GiroIzquierda();
 		break;
 
 	case 0B011000000:
-#ifdef DEBUG
-		Serial.print("-5  ");
-#endif // DEBUG
-		error = -5;
+		#ifdef DEBUG
+			Serial.print("-5  ");
+		#endif // DEBUG
+		GiroIzquierda();
 		break;
 
 	case 0B010000000:
-#ifdef DEBUG
-		Serial.print("-6  ");
-#endif // DEBUG
-		error = -6;
+		#ifdef DEBUG
+			Serial.print("-6  ");
+		#endif // DEBUG
+		GiroIzquierda();
 		break;
 
 	case 0B110000000:
-#ifdef DEBUG
-		Serial.print("-7  ");
-#endif // DEBUG
-		error = -7;
+		#ifdef DEBUG
+			Serial.print("-7  ");
+		#endif // DEBUG
+		GiroIzquierda();
 		break;
 
 	case 0B100000000:
-#ifdef DEBUG
-		Serial.print("-8  ");
-#endif // DEBUG
-		error = -8;
+		#ifdef DEBUG
+			Serial.print("-8  ");
+		#endif // DEBUG
+		GiroIzquierda();
 		break;
 
 	default:
-#ifdef DEBUG
-		Serial.print("Fuera  ");
-#endif // DEBUG
+		#ifdef DEBUG
+			Serial.print("Fuera  ");
+		#endif // DEBUG
+		Parar();
 		break;
 	}
 
 }
 
-void Motores()
+void Avanzar()
 {
-
 	digitalWrite(InputD[0], HIGH);
 	digitalWrite(InputD[1], LOW);
+
 	digitalWrite(InputI[0], HIGH);
 	digitalWrite(InputI[1], LOW);
 
-	analogWrite(enableD, control);
-	analogWrite(enableI, control1);
+	digitalWrite(enableD, HIGH);
+	digitalWrite(enableI, HIGH);
+}
 
-	if (control < 0)
-	{
-		control = abs(control);
-		digitalWrite(InputI[0], LOW);
-		digitalWrite(InputI[1], HIGH);
-	}
+void GiroDerecha()
+{
+	digitalWrite(InputD[0], HIGH);
+	digitalWrite(InputD[1], HIGH);
 
-	else if (control1 < 0)
-	{
-		control1 = abs(control1);
-		digitalWrite(InputD[0], LOW);
-		digitalWrite(InputD[1], HIGH);
-	}
+	digitalWrite(InputI[0], HIGH);
+	digitalWrite(InputI[1], LOW);
 
+	digitalWrite(enableD, HIGH);
+	digitalWrite(enableI, HIGH);
+}
+
+void GiroIzquierda()
+{
+	digitalWrite(InputD[0], HIGH);
+	digitalWrite(InputD[1], LOW);
+
+	digitalWrite(InputI[0], HIGH);
+	digitalWrite(InputI[1], HIGH);
+
+	digitalWrite(enableD, HIGH);
+	digitalWrite(enableI, HIGH);
+}
+
+void Parar()
+{
+	digitalWrite(InputD[0], HIGH);
+	digitalWrite(InputD[1], HIGH);
+
+	digitalWrite(InputI[0], HIGH);
+	digitalWrite(InputI[1], HIGH);
+
+	digitalWrite(enableD, HIGH);
+	digitalWrite(enableI, HIGH);
 }
